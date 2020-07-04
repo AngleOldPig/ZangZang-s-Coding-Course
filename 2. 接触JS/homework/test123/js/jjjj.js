9600a95d0b1747e976e3f2b79c133bcc145d6594
lsyvlr <67490625+lsyvlr@users.noreply.github.com>
2020-07-04 03:09:55 +0200

    homework2

---
+++ 2. 接触JS/homework/test123/js/jjjj.js
@@ -0,0 +1,25 @@
+
+            setTimeout(wakeUpUser, 1000);
+            function wakeUpUser(){
+                alert("不许套娃");
+            }
+
+
+var x = 30;
+if (x>20) {
+    alert("Not enough");
+}
+else 
+    alert("Buy buy buy!");
+
+
+
+var arr = [4,50,28,63];
+var i = 0;
+for(var j = 0; j < arr.length; j++) {
+    i = i + arr[j];
+}
+alert(i);
+
+
+
